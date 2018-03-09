26cf7e28cadc13f839577acb8f775afd9046ba20
Arni Magnusson <arnima@hafro.is>
2018-03-09 09:44:44 +0100

    Rename tutorial-01 -> README

--- tutorial-01/README.R
+++ tutorial-01/README.R
@@ -1,10 +1,10 @@
-## R script to convert tutorial-01.Rmd -> tutorial-01.md
+## R script to convert README.Rmd -> README.md
 
 ## To get TOC and numbered sections, just create HTML and call it .md
-rmarkdown::render("tutorial-01.Rmd", output_file="tutorial-01.md")
+rmarkdown::render("README.Rmd", output_file="README.md")
 
 ## Comment out doctype and title to make GitHub Markdown happy
-md <- readLines("tutorial-01.md")
+md <- readLines("README.md")
 md[md=="<!DOCTYPE html>"] <- "<!-- <!DOCTYPE html> -->"
 md <- gsub("(<title>.*</title>)", "<!-- \\1 -->", md)
-writeLines(md, "tutorial-01.md")
+writeLines(md, "README.md")
