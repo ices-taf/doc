fb1631cad6f8d8da3a34141ff8edc714fd374354
Colin Millar <colin.millar@ices.dk>
2020-10-23 17:28:26 +0200

    Update vms.R

--- datasets/vms.R
+++ datasets/vms.R
@@ -9,5 +9,24 @@
 #' @tafAccess Restricted
 #' @tafSource script
 
+library(icesTAF)
 library(icesVMS)
+library(icesVocab)
 
+# find the ecoregion of interest
+ecoregions <-
+  gsub(
+    " Ecoregion", "",
+    getCodeList("Ecoregion")$Description
+  )
+
+baltic_eco <- grep("Baltic", ecoregions, value = TRUE)
+
+# download
+# note to access download a token for the current R session using
+# update_token({ices username})
+
+vms <- get_vms(ecoregion = baltic_eco)
+
+# save
+write.taf(vms)
