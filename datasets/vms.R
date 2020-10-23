deddcbb8dda10bf94e98dec2dc3ce9029d6fba02
Colin Millar <colin.millar@ices.dk>
2020-10-23 17:04:00 +0200

    Create vms.R

---
+++ datasets/vms.R
@@ -0,0 +1,13 @@
+#' VMS data from the ICES VMS and Logbook Database
+#'
+#' VMS data from the ICES VMS and Logbook Database
+#'
+#' @name vms
+#' @format csv file
+#' @tafOriginator ICES
+#' @tafYear 2020
+#' @tafAccess Restricted
+#' @tafSource script
+
+library(icesVMS)
+
